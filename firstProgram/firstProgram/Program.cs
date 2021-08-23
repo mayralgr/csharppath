91bacfca39f89e2911d72ecbbd807cea1ac0fa78
mayralgr <ml17ft@gmail.com>
2021-08-22 21:42:12 -0500

    Add simple types

--- firstProgram/firstProgram/Program.cs
+++ firstProgram/firstProgram/Program.cs
@@ -6,7 +6,15 @@ namespace firstProgram
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            float result = FahrenheoToCelsius(76);
+            Console.WriteLine("Hello World! {0}", result);
+
+        }
+
+        static float FahrenheoToCelsius(float temperatureFahrenheit)
+        {
+            float temperatureCelsius = (temperatureFahrenheit - 32) / 1.8f;
+            return temperatureCelsius;
         }
     }
 }
