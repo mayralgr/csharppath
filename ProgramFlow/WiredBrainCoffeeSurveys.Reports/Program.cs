ddda9858d4aef632be5bedf6416ee2a325dd5601
mayralgr <ml17ft@gmail.com>
2021-08-24 18:35:37 -0500

    Add program flow

---
+++ ProgramFlow/WiredBrainCoffeeSurveys.Reports/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace WiredBrainCoffeeSurveys.Reports
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double responseRate = Q1Results.NumberResponded / Q1Results.NumberSurveyed;
+            double unansweredCount = Q1Results.NumberSurveyed - Q1Results.NumberResponded;
+
+            double overallScore = (Q1Results.ServiceScore +Q1Results.CoffeeScore + Q1Results.FoodScore + Q1Results.PriceScore) / 4;
+
+
+            Console.WriteLine($"Response Percentage: {responseRate}");
+            Console.WriteLine($"Unanswered surveys: {unansweredCount}");
+
+            bool higherCoffeScore = Q1Results.CoffeeScore > Q1Results.FoodScore;
+            bool customerRecommend = Q1Results.WouldRecommend >=7;
+            bool noGranolaYesCappucino = Q1Results.LeastFavoriteProduct == "Granola" && Q1Results.FavoriteProduct == "Cappucino";
+
+            Console.WriteLine($"Coofee Score Higher Than Food: {higherCoffeScore}");
+            Console.WriteLine($"customes Would Recommend Us: {customerRecommend}");
+            Console.WriteLine($"Hate Grano, Love Capuccino: {noGranolaYesCappucino}");
+
+        }
+    }
+}
