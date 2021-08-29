14bd6123dd4cdbaa11f468018019fe9ae2876518
mayralgr <ml17ft@gmail.com>
2021-08-28 19:08:33 -0500

    Add types course

---
+++ BethanyPieShopHRM/BethanyPieShopHRM/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BethanyPieShopHRM
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int montlyWage = 1234;
+            int months = 12, bonus = 1000;
+            bool isActive = true;
+            double rating = 99.25;
+
+            // byte numberOfEmployees = 300;
+            // montlyWage = true;
+
+            /**
+             * Operators
+             *   +
+             *   =
+             *   logical
+             *   asigned
+             */
+
+            Console.ReadLine();
+
+        }
+    }
+}
