15d30615217e17eb7b797579139a8c91d4c5589c
mayralgr <ml17ft@gmail.com>
2021-09-10 21:56:22 -0500

    Add service reader

---
+++ Extensibility/PeopleViewer/Controllers/PeopleController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PersonReader.Interface;
+using PersonReader.Service;
+using System.Collections.Generic;
+
+namespace PeopleViewer.Controllers
+{
+    public class PeopleController : Controller
+    {
+        public IActionResult UseService()
+        {
+            IPersonReader reader = new ServiceReader();
+            IEnumerable<Person> people = reader.GetPeople();
+            ViewData["ReaderType"] = reader.GetType().ToString();
+
+            ViewData["Title"] = "Using a Web Service";
+            return View("Index", people);
+        }
+
+        public IActionResult UseCSV()
+        {
+            ViewData["Title"] = "Using a CSV File";
+            return View("Index", new List<Person>());
+        }
+
+        public IActionResult UseSQL()
+        {
+            ViewData["Title"] = "Using a SQL Database";
+            return View("Index", new List<Person>());
+        }
+    }
+}
