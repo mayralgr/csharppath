ce0f27c057c470e59ec5a1127bb25a6d14216e50
mayralgr <ml17ft@gmail.com>
2021-09-19 21:03:20 -0500

    Add default implementation in interfaces

---
+++ DefaultImplementation/People.Library/IPeopleLogger.cs
@@ -0,0 +1,26 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+
+namespace People.Library
+{
+    public enum PeopleLogLevel
+    {
+        Critical,
+        Error,
+        Warning,
+        Information,
+        Debug,
+        Trace
+    }
+
+    public interface IPeopleLogger
+    {
+        public void Log(PeopleLogLevel level, string message);
+
+        public void LogException(Exception ex)
+        {
+            Log(PeopleLogLevel.Error, ex.Message);
+        }
+    }
+}
