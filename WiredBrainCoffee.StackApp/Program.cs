cfb70c2284b8fcb6f2371950bc58bba115a5d5b7
mayralgr <ml17ft@gmail.com>
2021-10-02 16:25:51 -0500

    Add stack project with doubles

---
+++ WiredBrainCoffee.StackApp/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace WiredBrainCoffee.StackApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StackDoubles();
+            StackString();
+            Console.ReadLine();
+        }
+
+        
+        private static void StackDoubles()
+        {
+            var stack = new SimpleStack();
+            stack.Push(1.2);
+            stack.Push(2.8);
+            stack.Push(3.0);
+
+            double sum = 0.0;
+
+            while (stack.Count > 0)
+            {
+                double item = stack.Pop();
+                Console.WriteLine($"Item: {item}");
+                sum += item;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+
+        private static void StackString()
+        {
+            var stack = new SimpleStack();
+            stack.Push("Wired brain coffee");
+            stack.Push("Pluralsight");
+        }
+    }
+}
